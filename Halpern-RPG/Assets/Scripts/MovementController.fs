8236ae72b6c6640ed849718b69012d227ab00362
Christopher <chrispritchard.nz@gmail.com>
2020-01-09 20:36:53 +1300

    started experimentation with scripts

---
+++ Halpern-RPG/Assets/Scripts/MovementController.fs
@@ -0,0 +1,12 @@
+﻿namespace HalpernRPG
+
+open UnityEngine
+
+type NewBehaviourScript() = 
+    inherit MonoBehaviour()
+
+    // Start is called before the first frame update
+    member _.Start() = ()
+
+    // Update is called once per frame
+    member _.Update() = ()
