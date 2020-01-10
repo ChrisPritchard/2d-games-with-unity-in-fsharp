2690adb77b9a3f31bb02e1b8456c5de92e974351
Christopher <chrispritchard.nz@gmail.com>
2020-01-11 09:10:28 +1300

    movement and animations coincides with the end of chapter 3

--- Scripts/MovementController.fs
+++ Scripts/MovementController.fs
@@ -7,19 +7,36 @@ type MovementController() as this =
 
     [<DefaultValue>]val mutable movementSpeed : float32
 
+    do
+        this.movementSpeed <- 3.f
+
     let mutable movement = Vector2()
     let mutable rb2d = Unchecked.defaultof<Rigidbody2D>
+    let mutable animator = Unchecked.defaultof<Animator>
 
-    do
-        this.movementSpeed <- float32 3.
+    let updateState () =
+        let setState n = animator.SetInteger ("AnimationState", n)
 
-    member this.Start() = 
-        rb2d <- this.GetComponent<Rigidbody2D>()
+        if movement.x > 0.f then setState 1
+        elif movement.y > 0.f then setState 2
+        elif movement.x < 0.f then setState 3
+        elif movement.y < 0.f then setState 4
+        else setState 0
 
-    member this.FixedUpdate() =
-
-        movement.x <- Input.GetAxisRaw("Horizontal")
-        movement.y <- Input.GetAxisRaw("Vertical")
+    let moveCharacter () =
+        movement.x <- Input.GetAxisRaw "Horizontal"
+        movement.y <- Input.GetAxisRaw "Vertical"
         movement.Normalize ()
-
+        
         rb2d.velocity <- movement * this.movementSpeed
+
+    member __.Start() = 
+        rb2d <- __.GetComponent<Rigidbody2D>()
+        animator <- __.GetComponent<Animator>()
+
+    member _.Update() =
+        updateState ()
+
+    member _.FixedUpdate() =
+        moveCharacter ()
+        
