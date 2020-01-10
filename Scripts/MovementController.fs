d7a5f1ad3250bd298895afb230be59e66e7bf5ac
Christopher <chrispritchard.nz@gmail.com>
2020-01-11 08:05:01 +1300

    switched copy command to use the cross platform msbuild version

--- Scripts/MovementController.fs
+++ Scripts/MovementController.fs
@@ -6,7 +6,7 @@ type MovementController() =
     inherit MonoBehaviour()
 
     // Start is called before the first frame update
-    member _.Start() = Debug.Log("F# Started with watch")
+    member _.Start() = Debug.Log("F# Started with watch and msbuild copy")
 
     // Update is called once per frame
     member _.Update() = ()
