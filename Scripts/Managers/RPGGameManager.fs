26f1f99de4ce62f0943495a41b8b4b393b4641ca
Christopher <chrispritchard.nz@gmail.com>
2020-01-12 08:35:57 +1300

    started work on an rpg game manager

---
+++ Scripts/Managers/RPGGameManager.fs
@@ -0,0 +1,20 @@
+﻿namespace HalpernRPG.Managers
+
+open UnityEngine
+
+[<AllowNullLiteral>]
+type RPGGameManager() =
+    inherit MonoBehaviour()
+
+    static let mutable sharedInstance = Unchecked.defaultof<_>
+
+    let setupScene () = ()
+
+    member this.Awake () =
+        if sharedInstance <> this && not (isNull sharedInstance) then
+            MonoBehaviour.Destroy this.gameObject
+        else
+            sharedInstance <- this
+
+    member _.Start () =
+        setupScene ()
