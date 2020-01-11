a7db81570ba6c35d2b780ad82015d08549099b69
Christopher <chrispritchard.nz@gmail.com>
2020-01-11 15:46:42 +1300

    added a pickup-able coin

--- Scripts/MonoBehaviours/Player.fs
+++ Scripts/MonoBehaviours/Player.fs
@@ -1,6 +1,10 @@
 ﻿namespace HalpernRPG.MonoBehaviours
 
+open UnityEngine
+
 type Player() = 
     inherit Character()
 
-        
+    member __.OnTriggerEnter2D (collision: Collider2D) =
+        if collision.gameObject.CompareTag "CanBePickedUp" then
+            collision.gameObject.SetActive false
