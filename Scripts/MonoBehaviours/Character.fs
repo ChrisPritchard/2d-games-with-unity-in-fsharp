e2a795b75d3ce59b755bffd55b7ff71f405837b2
Christopher <chrispritchard.nz@gmail.com>
2020-01-12 13:13:52 +1300

    basic enemy class

--- Scripts/MonoBehaviours/Character.fs
+++ Scripts/MonoBehaviours/Character.fs
@@ -12,4 +12,6 @@ type Character() =
     [<DefaultValue>]val mutable hitPoints : HitPoints
     [<DefaultValue>]val mutable maxHitPoints : float32
 
-        
+    abstract member KillCharacter: unit -> unit
+    default this.KillCharacter () =
+        MonoBehaviour.Destroy this.gameObject
---
+++ Scripts/MonoBehaviours/Enemy.fs
@@ -0,0 +1,9 @@
+﻿namespace HalpernRPG.MonoBehaviours
+
+open UnityEngine
+open HalpernRPG.ScriptableObjects
+
+type Enemy() = 
+    inherit Character()
+
+    [<DefaultValue>]val mutable hitPoints : float32
