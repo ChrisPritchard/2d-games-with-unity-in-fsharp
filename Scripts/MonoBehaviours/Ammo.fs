576e157cbf1d8b9aaa11ad321fd21b7399585d99
Christopher <chrispritchard.nz@gmail.com>
2020-01-12 17:17:33 +1300

    created the ammo prefab with script

---
+++ Scripts/MonoBehaviours/Ammo.fs
@@ -0,0 +1,17 @@
+namespace HalpernRPG.MonoBehaviours
+
+open UnityEngine
+
+[<AllowNullLiteral>]
+type Ammo() =
+    inherit MonoBehaviour()
+
+    [<DefaultValue>]val mutable damageInflicted : float32
+
+    member this.OnTriggerEnter2D (collider: Collider2D) =
+        match collider with
+        | :? BoxCollider2D as boxCollider ->
+            let enemy = collider.gameObject.GetComponent<Enemy>()
+            this.StartCoroutine (enemy.DamageCharacter this.damageInflicted 0.f) |> ignore
+            this.gameObject.SetActive false
+        | _ -> ()
